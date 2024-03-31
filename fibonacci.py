44e74b9f7dbba64b805203463ba2453ddf88a8fb
MariiaTomkiv <mariia.tomkiv@gmail.com>
2024-03-31 20:40:01 +0300

    Task_1_change

--- fibonacci.py
+++ fibonacci.py
@@ -1,17 +1,17 @@
 def caching_fibonacci(n):
-    cash = {}
+    cache = {}
     
     def fibonacci(n):
         if n <= 0:
             return 0
-        if n == 1:
-            return 1
-        for n in range(2, n):
-            if n in cash:
-                return cash[n]
-            cash[n] = fibonacci(n - 1) + fibonacci(n - 2)
-        return cash[n]
+        elif n == 1:
+             return 1
+        else:
+            for n in range(2, n):
+                if n in cache:
+                    return cache[n]
+                else:
+                    cache[n] = cache[n-1] + cache[n-2]
+        return cache[n]
+    
     return fibonacci
-
-fib = caching_fibonacci
-print(fib(1))
